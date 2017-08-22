91d8be2c53715e383f8982a03611faeadf0d0a01
Fabian Hagengers <fabian.hagengers@web.de>
2017-08-22 10:44:34 +0200

    Datenbankzugang hinzugefügt und login.css erstellt

--- Sourcen/login.php
+++ Sourcen/login.php
@@ -9,17 +9,30 @@
 
     <!-- CSS Includes -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/login.css">
   </head>
 
   <!-- BODY -->
   <body>
 
-    <h1>Hello, world!</h1>
+    <!-- Wrapper -->
+    <div class="container">
+
+      <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+
+        <!-- Login-Card -->
+        <div id="login-card">
+
+        </div>
+
+      </div>
+
+    </div>
 
     <!-- Javascript Includes -->
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 
   </body>
-  
+
 </html>
