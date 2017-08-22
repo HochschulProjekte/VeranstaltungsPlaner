aa0145300efb0045174bc0b861249b71b0eeb1ea
Fabian Hagengers <fabian.hagengers@web.de>
2017-08-22 10:12:26 +0200

    Sourcen und Dokumente hinzugefügt

---
+++ Sourcen/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Meta Tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSS Includes -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- Javascript Includes -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
