5f96eae7ae7a450500dbdb7eba19ae6e92e94f93
Fabian Hagengers <fabian.hagengers@web.de>
2017-08-22 12:09:21 +0200

    FtpZugang

--- Sourcen/login.php
+++ Sourcen/login.php
@@ -1,20 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+
+  <!-- HEAD -->
   <head>
-    <!-- Required meta tags -->
+    <!-- Meta Tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
+    <!-- CSS Includes -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/login.css">
   </head>
-  <body>
-    <h1>Hello, world!</h1>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <!-- BODY -->
+  <body>
+
+    <!-- Wrapper -->
+    <div class="container-fluid">
+
+      <div class="row justify-content-center">
+
+        <div class="col-12 col-sm-10 col-md-6 col-lg-6 col-xl-6">
+
+          <!-- Login-Card -->
+          <div id="login-card">
+
+            <h1>Veranstaltungsplaner</h1>
+
+            <hr>
+
+            <div class="input-group">
+              <div class="input-group-addon"><i class="fa fa-user"></i></div>
+              <input type="text" class="form-control" id="login-username" name="login-username" placeholder="Username">
+            </div>
+
+            <div class="input-group">
+              <div class="input-group-addon"><i class="fa fa-user"></i></div>
+              <input type="text" class="form-control" id="login-username" name="login-username" placeholder="Username">
+            </div>
+
+          </div>
+        
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Javascript Includes -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
   </body>
+
 </html>
